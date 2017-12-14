3733d93dd8643d6b6152e13b9946e44447328190
Gaurang Deshpande <deshpande.ga@husky.neu.edu>
2017-12-13 22:54:31 -0500

    added functionality to add event to database

---
+++ event-action.js
@@ -0,0 +1,13 @@
+// const getEvents = require('./actions/getEvent');
+// const getEventById = require('./actions/getEventById');
+// const putEventById = require('./actions/putEventById');
+const postEvent = require('./actions/postEvent');
+// const deleteEvent = require('./actions/deleteEvent');
+// const addPayment = require('./actions/addPaymentMethod');
+
+// exports.getEvent = getEvent;
+// exports.getEventById = getEventById;
+// exports.putEventById = putEventById;
+exports.postEvent = postEvent;
+// exports.deleteEvent = deleteEvent;
+// exports.addPayment = addPayment;
