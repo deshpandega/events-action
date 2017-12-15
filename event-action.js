67cd42c1a1b6d4cd398360b98755e28549636371
Gaurang <gaurangdeshpande89@gmail.com>
2017-12-15 04:10:44 -0500

    Added service: Search Events by hobby. Only 1 hobby to be used to search events

--- event-action.js
+++ event-action.js
@@ -1,12 +1,12 @@
 const getEvents = require('./actions/getEvents');
-// const getEventById = require('./actions/getEventById');
+const searchEventsByHobby = require('./actions/searchEventsByHobby');
 // const putEventById = require('./actions/putEventById');
 const postEvent = require('./actions/postEvent');
 // const deleteEvent = require('./actions/deleteEvent');
 // const addPayment = require('./actions/addPaymentMethod');
 
 exports.getEvents = getEvents;
-// exports.getEventById = getEventById;
+exports.searchEventsByHobby = searchEventsByHobby;
 // exports.putEventById = putEventById;
 exports.postEvent = postEvent;
 // exports.deleteEvent = deleteEvent;
