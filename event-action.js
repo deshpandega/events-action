dcf3e2d72e87a5efe2d9ba61d35658ea601ccf65
Gaurang <gaurangdeshpande89@gmail.com>
2017-12-15 19:45:46 -0500

    added getRegisteredEvents service

--- event-action.js
+++ event-action.js
@@ -3,11 +3,11 @@ const searchEventsByHobby = require('./actions/searchEventsByHobby');
 // const putEventById = require('./actions/putEventById');
 const postEvent = require('./actions/postEvent');
 const getHostedEvents = require('./actions/getHostedEvents');
-// const addPayment = require('./actions/addPaymentMethod');
+const getRegisteredEvents = require('./actions/getRegisteredEvents');
 
 exports.getEvents = getEvents;
 exports.searchEventsByHobby = searchEventsByHobby;
 // exports.putEventById = putEventById;
 exports.postEvent = postEvent;
 exports.getHostedEvents = getHostedEvents;
-// exports.addPayment = addPayment;
+exports.getRegisteredEvents = getRegisteredEvents;
